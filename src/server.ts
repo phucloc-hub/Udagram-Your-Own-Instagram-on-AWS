44a9857d84be2068025a9b99f7fe636ad1f190a9
LocTP <trphucloc@gmail.com>
2023-06-16 11:11:25 +0700

    fix comment

--- src/server.ts
+++ src/server.ts
@@ -28,8 +28,8 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-  app.get( "/filteredimage", async ( req, res ) => {
-    let image_url = req.query.image_url;
+  app.get( "/filteredimage", async ( req :express.Request, res :express.Response) => {
+    let image_url :string = req.query.image_url;
 
     if (!image_url)
     {
